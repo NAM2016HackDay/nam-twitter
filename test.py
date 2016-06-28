be0092f02d92945fe252ca47bf128224e53ae21f
Daniel Williams <mail@daniel-williams.co.uk>
2016-06-28 10:16:37 +0100

    initial commit.

---
+++ test.py
@@ -0,0 +1,15 @@
+import tweepy
+
+import credentials
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print tweet.text
+
+
+api.update_status("This is a #test! Yay!")
